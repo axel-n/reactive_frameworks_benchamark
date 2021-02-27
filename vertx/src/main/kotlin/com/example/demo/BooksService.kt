3b5d248154e36f17e903fa0380120aaab658dd87
Alex Anoshkin <Anoshkin.allex@gmail.com>
2021-02-27 17:20:16 +0300

    add delay for emulate await from database or another service

--- vertx/src/main/kotlin/com/example/demo/BooksService.kt
+++ vertx/src/main/kotlin/com/example/demo/BooksService.kt
@@ -1,12 +1,19 @@
 package com.example.demo
 
 import java.util.UUID
+import kotlin.random.Random
 
 class BooksService {
-     suspend fun findBookById(uuid: UUID): BookDto {
+    suspend fun findBookById(uuid: UUID): BookDto {
+
+        getRandomDelay(150, 200)
+
         return BookDto(
             id = UUID.randomUUID(),
             name = "book name"
         )
     }
+
+    private fun getRandomDelay(min: Long, max: Long): Long =
+        Random.nextLong(min, max)
 }
